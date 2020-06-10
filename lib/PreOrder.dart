a67358feb917487598580c26ee1c59526d16d38c
Matthew <matthewkenyondu@gmail.com>
2020-06-10 22:28:40 +0800

    First Commit

--- lib/PreOrder.dart
+++ lib/PreOrder.dart
@@ -8,44 +8,51 @@ class _PreOrder extends State<PreOrder> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-        child: Column(children: <Widget>[
-          Container(width: MediaQuery.of(context).size.width*0.5, child:
-      Card(
-          elevation: 5,
-          child: Column(children: <Widget>[
-            Image.asset('assets/Rectangle.png'),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('TGI Fridays'),
-                  Container(
-                      child: Row(children: <Widget>[
-                    Text('4.8'),
-                    Image.asset('assets/Star 7.png')
-                  ]))
-                ]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-              Container(child: Row(children:<Widget>[Image.asset(
-                  "assets/Peso.png"
-                ),
-          Image.asset(
-              "assets/Peso.png"
-          ),
-          Image.asset(
-          "assets/UnPeso.png"
-      ),])),
-                  Container(padding: EdgeInsets.symmetric(horizontal: 4), child:Text('2.5km')),
-                ImageIcon(
-                  AssetImage("assets/Destination.png"), color: Colors.red,
-                ),
-                Text('Bonifacio High Street')
-              ]
-            ),
-            SizedBox(height:10),
-            Text('American, Steaks, Burgers')
-          ]))),
+    return Column(children: <Widget>[
+      Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          child: ListView.builder(
+              itemCount: 7,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, i) {
+                return Card(
+                        elevation: 5,
+                        child: Container(height: double.infinity, child: Column(children: <Widget>[
+                          Image.asset('assets/Rectangle.png'),
+                          Container( width: MediaQuery.of(context).size.width*0.5, child: Row(
+                              mainAxisSize:MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('TGI Fridays'),
+                                Container(
+                                    child: Row(children: <Widget>[
+                                  Text('4.8'),
+                                  Image.asset('assets/Star 7.png')
+                                ]))
+                              ])),
+                          Row(
+                              children: <Widget>[
+                                Container(
+                                    child: Row(children: <Widget>[
+                                  Image.asset("assets/Peso.png"),
+                                  Image.asset("assets/Peso.png"),
+                                  Image.asset("assets/UnPeso.png"),
+                                ])),
+                                Container(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 4),
+                                    child: Text('2.5km')),
+                                ImageIcon(
+                                  AssetImage("assets/Destination.png"),
+                                  color: Colors.red,
+                                ),
+                                Text('Bonifacio High Street')
+                              ]),
+                          Align(alignment: Alignment.bottomCenter, child: Text('American, Steaks, Burgers'))
+                        ])));
+              })),
       Text('Order Again')
-    ]));
+    ]);
   }
 }
